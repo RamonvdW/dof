7bc147de61e6bf408040c5f84fc0897e1d812046
RamonvdW <nhb.ramonvdw@gmail.com>
2023-02-07 19:22:19 +0100

    Updated 3rd party packages

--- dof/requirements/compile.sh
+++ dof/requirements/compile.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-#  Copyright (c) 2021-2022 Ramon van der Winkel.
+#  Copyright (c) 2021-2023 Ramon van der Winkel.
 #  All rights reserved.
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
 # ga naar de directory waar het script staat
 cd $(dirname $0)
 
-rm requirements.txt
-pip-compile -q requirements.in
-
-rm requirements_dev.txt
-pip-compile -q requirements_dev.in
+for req in requirements requirements_dev
+do
+    OUT="$req.txt"
+    IN="$req.in"
+    echo "[INFO] Creating $OUT"
+    [ -f "$OUT" ] && rm "$OUT"
+    pip-compile --resolver=backtracking -q "$IN"
+done
 
 # end of file
 
